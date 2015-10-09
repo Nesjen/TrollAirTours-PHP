eafaade2ca95e42c04acbd4aabf03a1072d67450
Nesjen <ebnesjen@gmail.com>
2015-10-09 12:58:10 +0200

    link fix for Php version

--- TrollAirTours-PHP-V2/controller/controllers.php
+++ TrollAirTours-PHP-V2/controller/controllers.php
@@ -3,6 +3,6 @@
 // Include all controllers we have
 require_once("HomeController.php");
 require_once("CustomerController.php");
-
+require_once ("AboutController.php");
 // TODO - create other controllers as separate Controller subclasses
 // TODO - include other controllers here
